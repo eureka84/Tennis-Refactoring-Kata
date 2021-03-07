65eb21ba285134ae7345c464315ab5f08282fd30
Angelo Sciarra <angelosciarra@ymail.com>
2021-03-07 17:45:34 +0100

    refactor: extracted methods for each branch and gave a name to all the cases

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,44 +1,51 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var player1Score: Int = 0
+    private var player2Score: Int = 0
 
     override fun wonPoint(playerName: String) {
         if (playerName === player1Name)
-            m_score1 += 1
+            player1Score += 1
         else
-            m_score2 += 1
+            player2Score += 1
     }
 
-    override fun getScore(): String {
-        if (m_score1 == m_score2) {
-            return when (m_score1) {
-                0 -> "Love-All"
-                1 -> "Fifteen-All"
-                2 -> "Thirty-All"
-                else -> "Deuce"
-            }
-        }
-        if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
-            return if (minusResult == 1)
-                "Advantage $player1Name"
-            else if (minusResult == -1)
-                "Advantage $player2Name"
-            else if (minusResult >= 2)
-                "Win for $player1Name"
-            else
-                "Win for $player2Name"
-        }
+    override fun getScore(): String = when {
+        isTied() -> tiedScore()
+        eitherPlayersOver40() -> finalScore()
+        else -> runningGameScore()
+    }
 
+    private fun tiedScore(): String {
+        return when (player1Score) {
+            0 -> "Love-All"
+            1 -> "Fifteen-All"
+            2 -> "Thirty-All"
+            else -> "Deuce"
+        }
+    }
+
+    private fun finalScore(): String {
+        val minusResult = player1Score - player2Score
+        return if (minusResult == 1)
+            "Advantage $player1Name"
+        else if (minusResult == -1)
+            "Advantage $player2Name"
+        else if (minusResult >= 2)
+            "Win for $player1Name"
+        else
+            "Win for $player2Name"
+    }
+
+    private fun runningGameScore(): String {
         var score = ""
         var tempScore: Int
         for (i in 1..2) {
             if (i == 1)
-                tempScore = m_score1
+                tempScore = player1Score
             else {
                 score += "-"
-                tempScore = m_score2
+                tempScore = player2Score
             }
             when (tempScore) {
                 0 -> score += "Love"
@@ -49,4 +56,8 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         }
         return score
     }
+
+    private fun eitherPlayersOver40() = player1Score >= 4 || player2Score >= 4
+
+    private fun isTied() = player1Score == player2Score
 }
