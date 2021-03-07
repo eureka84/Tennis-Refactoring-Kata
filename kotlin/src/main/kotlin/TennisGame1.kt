a98bb5d558de9a8457e9ce7d6fd1297aa95ce8aa
Angelo Sciarra <angelosciarra@ymail.com>
2021-03-07 17:48:53 +0100

    refactored running game score

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -16,46 +16,35 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         else -> runningGameScore()
     }
 
-    private fun tiedScore(): String {
-        return when (player1Score) {
-            0 -> "Love-All"
-            1 -> "Fifteen-All"
-            2 -> "Thirty-All"
-            else -> "Deuce"
-        }
-    }
+    private fun tiedScore(): String =
+            when (player1Score) {
+                0 -> "Love-All"
+                1 -> "Fifteen-All"
+                2 -> "Thirty-All"
+                else -> "Deuce"
+            }
 
     private fun finalScore(): String {
         val minusResult = player1Score - player2Score
-        return if (minusResult == 1)
-            "Advantage $player1Name"
-        else if (minusResult == -1)
-            "Advantage $player2Name"
-        else if (minusResult >= 2)
-            "Win for $player1Name"
-        else
-            "Win for $player2Name"
+        return when {
+            minusResult == 1 -> "Advantage $player1Name"
+            minusResult == -1 -> "Advantage $player2Name"
+            minusResult >= 2 -> "Win for $player1Name"
+            else -> "Win for $player2Name"
+        }
     }
 
-    private fun runningGameScore(): String {
-        var score = ""
-        var tempScore: Int
-        for (i in 1..2) {
-            if (i == 1)
-                tempScore = player1Score
-            else {
-                score += "-"
-                tempScore = player2Score
-            }
+    private fun runningGameScore(): String =
+            "${scoreStringFor(player1Score)}-${scoreStringFor(player2Score)}"
+
+    private fun scoreStringFor(tempScore: Int) =
             when (tempScore) {
-                0 -> score += "Love"
-                1 -> score += "Fifteen"
-                2 -> score += "Thirty"
-                3 -> score += "Forty"
+                0 -> "Love"
+                1 -> "Fifteen"
+                2 -> "Thirty"
+                3 -> "Forty"
+                else -> ""
             }
-        }
-        return score
-    }
 
     private fun eitherPlayersOver40() = player1Score >= 4 || player2Score >= 4
 
