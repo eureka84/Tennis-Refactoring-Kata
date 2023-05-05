1e63cf08dbf8f07a79894c4a034edb8072797000
Alvaro Espuña <alvaro.espuna@glovoapp.com>
2023-05-05 17:11:59 +0200

    Use player1Name

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -4,7 +4,7 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
 	private var player2Score: Int = 0
 
 	override fun wonPoint(playerName: String) {
-		if (playerName === "player1")
+		if (playerName === player1Name)
 			player1Score += 1
 		else
 			player2Score += 1
