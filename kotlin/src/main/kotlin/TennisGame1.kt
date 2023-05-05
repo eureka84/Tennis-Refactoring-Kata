3546a53b5fb54c718f4442d0e86ed878f181afaf
Alvaro Espuña <alvaro.espuna@glovoapp.com>
2023-05-05 17:06:33 +0200

    Inline score

--- kotlin/src/main/kotlin/TennisGame1.kt
+++ kotlin/src/main/kotlin/TennisGame1.kt
@@ -1,51 +1,45 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var player1Score: Int = 0
-    private var player2Score: Int = 0
+	private var player1Score: Int = 0
+	private var player2Score: Int = 0
 
-    override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            player1Score += 1
-        else
-            player2Score += 1
-    }
+	override fun wonPoint(playerName: String) {
+		if (playerName === "player1")
+			player1Score += 1
+		else
+			player2Score += 1
+	}
 
-    override fun getScore(): String {
-        var score = ""
-        if (player1Score == player2Score) {
-            score = drawScore()
-        } else if (player1Score.isOver40() || player2Score.isOver40()) {
-            score = advantagesScore()
-        } else {
-            score = "${getScoreName(player1Score)}-${getScoreName(player2Score)}"
-        }
-        return score
-    }
+	override fun getScore(): String = when {
+		player1Score == player2Score -> drawScore()
+		player1Score.isOver40() || player2Score.isOver40() -> advantagesScore()
+		else -> "${getScoreName(player1Score)}-${getScoreName(player2Score)}"
+	}
 
-    private fun getScoreName(tempScore: Int) = when (tempScore) {
-        0 -> "Love"
-        1 -> "Fifteen"
-        2 -> "Thirty"
-        3 -> "Forty"
-        else -> throw IllegalArgumentException("Temp score cannot be greater than 3")
-    }
+	private fun getScoreName(tempScore: Int) = when (tempScore) {
+		0 -> "Love"
+		1 -> "Fifteen"
+		2 -> "Thirty"
+		3 -> "Forty"
+		else -> throw IllegalArgumentException("Temp score cannot be greater than 3")
+	}
 
-    private fun advantagesScore(): String {
-        val scoreDifference = player1Score - player2Score
-        return when {
-            scoreDifference == 1 -> "Advantage player1"
-            scoreDifference >= 2 -> "Win for player1"
-            scoreDifference == -1 -> "Advantage player2"
-            else -> "Win for player2"
-        }
-    }
+	private fun advantagesScore(): String {
+		val scoreDifference = player1Score - player2Score
+		return when {
+			scoreDifference == 1 -> "Advantage player1"
+			scoreDifference >= 2 -> "Win for player1"
+			scoreDifference == -1 -> "Advantage player2"
+			else -> "Win for player2"
+		}
+	}
 
-    private fun Int.isOver40() = this >= 4
+	private fun Int.isOver40() = this >= 4
 
-    private fun drawScore(): String = when (player1Score) {
-        0 -> "Love-All"
-        1 -> "Fifteen-All"
-        2 -> "Thirty-All"
-        else -> "Deuce"
-    }
+	private fun drawScore(): String = when (player1Score) {
+		0 -> "Love-All"
+		1 -> "Fifteen-All"
+		2 -> "Thirty-All"
+		else -> "Deuce"
+	}
 }
